d394a1a9526b33b896c37fb0277fe3d87d869fc5
Chenyuxuan-walker <yuxuanc108@gmail.com>
2026-03-24 21:47:06 +0800

    求最小整数n
    第一次作业题1

---
+++ Day04--求最小的正整数n.c
@@ -0,0 +1,32 @@
+//输入一个正整数n，使得n/2是某个数的平方，n/3是某个数的立方
+#include<stdio.h>
+#include<math.h>
+#include <stdbool.h>
+bool issquare(long long num)
+{
+    if(num<0)
+        return false;
+    long long root=(long long)round(sqrt(num));
+    return (root*root == num);
+}
+bool iscube(long long num)
+{
+    if(num<0)
+        return false;
+    long long root=(long long)round(cbrt(num));
+    return (root*root*root == num);
+}
+int main()
+{
+    for(long long n=6; ;n+=6)
+    {
+        if(issquare(n/2) && iscube(n/3))
+        {
+            printf("最小的正整数为%lld\n",n);
+            printf("%lld是%lld的平方\n",n/2,(long long)round((sqrt(n/2))));
+            printf("%lld是%lld的立方\n",n/3,(long long)round((cbrt(n/3))));
+            break;
+        }
+    }
+    return 0;
+}
