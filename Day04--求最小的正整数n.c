e1e76a1943e92ac217010a6e588c3b40c3e98a9d
Chenyuxuan-walker <yuxuanc108@gmail.com>
2026-03-24 23:00:13 +0800

    Update Day04--求最小的正整数n.c

--- Day04--求最小的正整数n.c
+++ Day04--求最小的正整数n.c
@@ -2,14 +2,14 @@
 #include<stdio.h>
 #include<math.h>
 #include <stdbool.h>
-bool issquare(long long num)
+bool issquare(long long num)//定义判断平方数函数
 {
     if(num<0)
         return false;
     long long root=(long long)round(sqrt(num));
     return (root*root == num);
 }
-bool iscube(long long num)
+bool iscube(long long num)//定义判断立方数函数
 {
     if(num<0)
         return false;
@@ -18,9 +18,9 @@ bool iscube(long long num)
 }
 int main()
 {
-    for(long long n=6; ;n+=6)
+    for(long long n=6; ;n+=6)//能被2和3整除的数必然被6整除
     {
-        if(issquare(n/2) && iscube(n/3))
+        if(issquare(n/2) && iscube(n/3))//两者均为正，则即为最小整数
         {
             printf("最小的正整数为%lld\n",n);
             printf("%lld是%lld的平方\n",n/2,(long long)round((sqrt(n/2))));
