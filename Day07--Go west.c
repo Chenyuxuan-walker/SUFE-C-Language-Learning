73b0652a9ab6d553d098b37c69bc1da0628d7731
Chenyuxuan-walker <yuxuanc108@gmail.com>
2026-04-09 16:09:44 +0800

    Go west young man!

---
+++ Day07--Go west.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main(void)
+{
+    char ch;
+//    scanf("%c",&ch);
+/*  while (ch != 'g')
+    {
+        printf ("%c",ch);
+        scanf("%c", &ch);
+    }
+    while (ch != 'g')
+    {
+        printf("%c",++ch);
+        scanf("%c",&ch);
+    }
+    do
+    {
+        scanf("%c", &ch);
+        printf("%c",ch);
+    } while (ch != 'g');
+*/
+    for(ch= '$';ch!='g';scanf("%c",&ch))
+        printf("%c",ch);
+    return 0;
+}
